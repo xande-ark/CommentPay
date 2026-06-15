939f979c9f41ab62f11a53c8590a0703df24a03a
Alexandre <alexandre@commentpay.com>
2026-06-15 15:49:56 -0300

    feat: adicionar logo e favicon customizados em todas as páginas e otimizar margens

--- check_db.js
+++ check_db.js
@@ -1,16 +1,8 @@
 require('dotenv').config();
 const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false }
-});
+const pool = new Pool({ connectionString: process.env.DATABASE_URL || 'postgres://default:A6gZ5hItNcbp@ep-red-bush-a4a06o0r.us-east-1.aws.neon.tech:5432/verceldb?sslmode=require' });
 
-pool.query("SELECT id, api_key_secret FROM peripheral_sites WHERE id = 'site-amorpg-123'")
-  .then(res => {
-    console.log(res.rows);
-    process.exit(0);
-  })
-  .catch(err => {
-    console.error(err);
-    process.exit(1);
-  });
+pool.query("SELECT id, email, google_sub, status FROM users WHERE email = 'webdesigner77rf@gmail.com'", (err, res) => {
+  console.log(err || res.rows);
+  process.exit();
+});
